b32aef52290ad992b466ef5b87f63ddda5f1f32c
leechiang.tan <leechiang.tan@univers.com>
2024-08-21 17:21:00 +0800

    refactor

--- player.py
+++ player.py
@@ -1,13 +1,10 @@
 from piece import Piece
 
+
 class Player:
     def __init__(self, piece):
         self.piece = piece
-    
+
     def play(self):
         moves = input('Enter your move (eg 78 is row 7 and column 8) or q(Q) to quit:')
         return moves
-
-
-
-
